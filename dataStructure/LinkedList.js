96ce44080550a2b6d9fbc7153aa5ad54453c70b2
jeongbaebang <jeongbaebang_dev@naver.com>
2024-08-06 00:59:03 +0900

    Add SingleLinkedList

---
+++ dataStructure/LinkedList.js
@@ -0,0 +1,87 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+class SingleLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  find(value) {
+    let currNode = this.head;
+
+    // 원하는 요소가 존재하지 않는 경우
+    while (currNode.value !== value) {
+      /// 다음 노드 이동
+      currNode = currNode.next;
+    }
+
+    return currNode;
+  }
+
+  append(newValue) {
+    const newNode = new Node(newValue);
+
+    // 첫번째 노드일때
+    if (this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      // 다른 포인터가 존재할때
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+  }
+
+  insert(node, newValue) {
+    const newNode = new Node(newValue);
+
+    newNode.next = node.next;
+    node.next = newNode;
+  }
+
+  remove(value) {
+    let prevNode = this.head;
+
+    // 값을 찾지 못한다면 다음 포인터 이동
+    while (prevNode.next.value !== value) {
+      prevNode = prevNode.next;
+    }
+
+    // 다음 포인터가 존재하는 경우
+    if (prevNode.next !== null) {
+      prevNode.next = prevNode.next.next;
+    }
+  }
+
+  display() {
+    let currNode = this.head;
+    let displayString = '[';
+
+    // 다음 포인터가 존재하지 않을 때까지 순회
+    while (currNode !== null) {
+      displayString += `${currNode.value}, `;
+      // 다음 포인터 이동
+      currNode = currNode.next;
+    }
+
+    displayString = displayString.substr(0, displayString.length - 2);
+    displayString += ']';
+
+    console.log(displayString);
+  }
+}
+
+const linkedList = new SingleLinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+linkedList.append(5);
+
+linkedList.display();
+console.log(linkedList.find(1))
